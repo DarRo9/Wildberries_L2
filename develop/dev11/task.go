465b2cae76309f9968ca707ab5deb4c299640e28
DarRo9 <124833469+DarRo9@users.noreply.github.com>
2023-11-06 01:03:49 +0300

    Update task.go

--- develop/dev11/task.go
+++ develop/dev11/task.go
@@ -45,150 +45,102 @@ import (
 	"time"
 )
 
-// Структура события
 type Event struct {
 	UserID      int    `json:"user_id"`
 	ID          int    `json:"id"`
-	Date        MyDate `json:"date"`
+	Date        Date                        `json:"date"`
 	Title       string `json:"title"`
 	Description string `json:"description"`
 }
 
-// Структура для получения конкретного события
+// ConcreteEvent структура для получения конкретного события
 type ConcreteEvent struct {
 	UserID int `json:"user_id"`
 	ID     int `json:"id"`
 }
 
-// Обертка для даты
-type MyDate struct {
-	date time.Time
-}
-
-// Хранилище для событий
-type EventStore struct {
+type EventRepository struct {
 	m map[int][]Event
 	*sync.RWMutex
 }
 
-// Собираем вместе сервер, логер и хранилище
-type Scope struct {
-	srv        *http.ServeMux
-	logger     Logger
-	eventStore EventStore
+type Date struct {
+	date time.Time
 }
 
-// Логер
 type Logger struct {
 	*log.Logger
 }
 
-// Инициалзирует scope
-func InitScope() *Scope {
+// Scope - сервер, логер и хранилище
+type Scope struct {
+	srv        *http.ServeMux
+	logger     Logger
+	EventRepository EventRepository
+}
+
+func CreateScope() *Scope {
 	return &Scope{
 		srv: http.NewServeMux(),
 		logger: Logger{
 			log.New(os.Stdout, "logger: ", log.Lshortfile),
 		},
-		eventStore: EventStore{
+		EventRepository: EventRepository{
 			m:       make(map[int][]Event),
 			RWMutex: new(sync.RWMutex),
 		},
 	}
 }
 
-// Настраивает роуты и запускает сервер
-func (s *Scope) startServer() {
-	s.srv.HandleFunc("/create_event", s.CreateEvent)
-	s.srv.HandleFunc("/update_event", s.UpdateEvent)
-	s.srv.HandleFunc("/delete_event", s.DeleteEvent)
+// startingServer настраивает роуты и запускает сервер
+func (scope *Scope) startingServer() {
+	scope.srv.HandleFunc("/create_event", scope.CreateEvent)
+	scope.srv.HandleFunc("/update_event", scope.UpdateEvent)
+	scope.srv.HandleFunc("/delete_event", scope.RemoveEvent)
 
-	s.srv.HandleFunc("/events_for_day", s.EventsForDay)
-	s.srv.HandleFunc("/events_for_week", s.EventsForWeek)
-	s.srv.HandleFunc("/events_for_month", s.EventsForMonth)
+	scope.srv.HandleFunc("/events_for_day", scope.DayEvents)
+	scope.srv.HandleFunc("/events_for_week", scope.WeekEvents)
+	scope.srv.HandleFunc("/events_for_month", scope.MonthEvents)
 
-	log.Fatal(http.ListenAndServe("localhost:"+os.Getenv("SERVERPORT"), s.srv))
+	log.Fatal(http.ListenAndServe("localhost:"+os.Getenv("SERVERPORT"), scope.srv))
 }
 
-// Отправляет заданную ошибку с заданным кодов статуса
-func sendError(w http.ResponseWriter, errorStr string, status int) {
-	response := struct {
-		Error string `json:"error"`
-	}{errorStr}
-	jsonResponse, err := json.Marshal(response)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-	w.WriteHeader(status)
-	w.Header().Set("Content-Type", "application/json")
-	_, _ = w.Write(jsonResponse)
-}
-
-// Отправляет результат запроса
-func sendResult(w http.ResponseWriter, resStr string, events []Event, status int) {
-	response := struct {
-		Result string  `json:"result"`
-		Events []Event `json:"events"`
-	}{resStr, events}
-	responseJson, err := json.Marshal(response)
-	if err != nil {
-		sendError(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	w.WriteHeader(status)
-	w.Header().Set("Content-Type", "application/json")
-	_, _ = w.Write(responseJson)
-}
-
-// Проверяет содержится ли событие в хранилище
-func (s *Scope) containsEvent(e Event) bool {
-	for _, v := range s.eventStore.m[e.UserID] {
-		if v.ID == e.ID {
-			return true
-		}
-	}
-	return false
-}
-
-// Создает новое событие и сохраняет его в хранилище
-func (s *Scope) CreateNewEvent(event Event) error {
-	s.eventStore.RWMutex.Lock()
-	defer s.eventStore.RWMutex.Unlock()
-	if s.containsEvent(event) {
+// CreateNewEvent создает новое событие и сохраняет его в хранилище
+func (scope *Scope) CreateNewEvent(event Event) error {
+	scope.EventRepository.RWMutex.Lock()
+	defer scope.EventRepository.RWMutex.Unlock()
+	if scope.checkEvent(event) {
 		return errors.New("duplicate event not allowed")
 	} else {
-		s.eventStore.m[event.UserID] = append(s.eventStore.m[event.UserID], event)
+		scope.EventRepository.m[event.UserID] = append(scope.EventRepository.m[event.UserID], event)
 	}
 	return nil
 }
 
-// Обработчик /create_event
-func (s *Scope) CreateEvent(w http.ResponseWriter, r *http.Request) {
-	s.logger.Println(r.URL)
+func (scope *Scope) CreateEvent(w http.ResponseWriter, r *http.Request) {
+	scope .logger.Println(r.URL)
 	if r.Method != http.MethodPost {
-		sendError(w, "Not correct method", http.StatusBadRequest)
+		sendErr(w, "Not correct method", http.StatusBadRequest)
 		return
 	}
 
-	event, err := parseJson(r)
+	event, err := parseJSON(r)
 
 	if err != nil {
-		sendError(w, err.Error(), http.StatusBadRequest)
+		sendErr(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-	if err := s.CreateNewEvent(event); err != nil {
-		sendError(w, err.Error(), http.StatusBadRequest)
+	if err := scope.CreateNewEvent(event); err != nil {
+		sendErr(w, err.Error(), http.StatusBadRequest)
 	}
-	sendResult(w, "Success", []Event{event}, http.StatusCreated)
+	sendRes(w, "Success", []Event{event}, http.StatusCreated)
 }
 
-// Обновляет существующее событие
-func (s *Scope) UpdateEventFunc(e Event) error {
-	s.eventStore.RWMutex.Lock()
-	defer s.eventStore.RWMutex.Unlock()
+func (scope  *Scope) UpdateEventFunc(e Event) error {
+	scope .EventRepository.RWMutex.Lock()
+	defer scope .EventRepository.RWMutex.Unlock()
 
-	events := s.eventStore.m[e.UserID]
+	events := scope .EventRepository.m[e.UserID]
 	for ind := 0; ind < len(events); ind++ {
 		if events[ind].ID == e.ID {
 			events[ind].Title = e.Title
@@ -200,78 +152,149 @@ func (s *Scope) UpdateEventFunc(e Event) error {
 	return errors.New("event not found")
 }
 
-// Обработчик /update_event
-func (s *Scope) UpdateEvent(w http.ResponseWriter, r *http.Request) {
-	s.logger.Println(r.URL)
+func (scope *Scope) UpdateEvent(w http.ResponseWriter, r *http.Request) {
+	scope.logger.Println(r.URL)
 	if r.Method != http.MethodPost {
-		sendError(w, "Not correct method", http.StatusBadRequest)
+		sendErr(w, "Not correct method", http.StatusBadRequest)
 		return
 	}
-	event, err := parseJson(r)
+	event, err := parseJSON(r)
 	if err != nil {
-		sendError(w, err.Error(), http.StatusBadRequest)
+		sendErr(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-	if EventIsValid(event) {
-		err = s.UpdateEventFunc(event)
+	if ValidEvents(event) {
+		err = scope.UpdateEventFunc(event)
 		if err != nil {
-			sendError(w, "Event not found", http.StatusInternalServerError)
+			sendErr(w, "Event not found", http.StatusInternalServerError)
 		} else {
-			sendResult(w, "Success", []Event{event}, http.StatusOK)
+			sendRes(w, "Success", []Event{event}, http.StatusOK)
 		}
 	} else {
-		sendError(w, "Not valid event", http.StatusBadRequest)
+		sendErr(w, "Not valid event", http.StatusBadRequest)
 	}
 }
 
-// Удаляет существующее событие
-func (s *Scope) DeleteEventFunc(cEvent ConcreteEvent) error {
-	s.eventStore.RWMutex.Lock()
-	defer s.eventStore.RWMutex.Unlock()
+func (scope *Scope) RemoveEventFunc(cEvent ConcreteEvent) error {
+	scope.EventRepository.RWMutex.Lock()
+	defer scope.EventRepository.RWMutex.Unlock()
 
-	events := s.eventStore.m[cEvent.UserID]
+	events := scope.EventRepository.m[cEvent.UserID]
 	for ind := 0; ind < len(events); ind++ {
 		if events[ind].ID == cEvent.ID {
-			s.eventStore.m[cEvent.UserID] = append(s.eventStore.m[cEvent.UserID][0:ind], s.eventStore.m[cEvent.UserID][ind+1:]...)
+			scope.EventRepository.m[cEvent.UserID] = append(scope.EventRepository.m[cEvent.UserID][0:ind], scope.EventRepository.m[cEvent.UserID][ind+1:]...)
 			return nil
 		}
 	}
 	return errors.New("event not found")
 }
 
-// Обработчик /delete_event
-func (s *Scope) DeleteEvent(w http.ResponseWriter, r *http.Request) {
-	s.logger.Println(r.URL)
+func (scope *Scope) RemoveEvent(w http.ResponseWriter, r *http.Request) {
+	scope.logger.Println(r.URL)
 	if r.Method != http.MethodPost {
-		sendError(w, "Not correct method", http.StatusBadRequest)
+		sendErr(w, "Not correct method", http.StatusBadRequest)
 		return
 	}
 
 	var cEvent ConcreteEvent
 	err := json.NewDecoder(r.Body).Decode(&cEvent)
 	if err != nil {
-		sendError(w, err.Error(), http.StatusBadRequest)
+		sendErr(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	err = s.DeleteEventFunc(cEvent)
+	err = scope.RemoveEventFunc(cEvent)
 	if err != nil {
-		sendError(w, err.Error(), http.StatusInternalServerError)
+		sendErr(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	sendResult(w, "Success", nil, http.StatusOK)
+	sendRes(w, "Success", nil, http.StatusOK)
 }
 
-// Возвращает события на заданный день
-func (s *Scope) EventsForDayFunc(userID int, date time.Time) ([]Event, error) {
-	s.eventStore.RWMutex.RLock()
-	s.eventStore.RWMutex.RUnlock()
+// sendErr отправляет заданную ошибку с заданным кодов статуса
+func sendErr(writer http.ResponseWriter, errorStr string, status int) {
+	response := struct {
+		Error string `json:"error"`
+	}{errorStr}
+	jsonResponse, err := json.Marshal(response)
+	if err != nil {
+		http.Error(writer, err.Error(), http.StatusInternalServerError)
+	}
+	writer.WriteHeader(status)
+	writer.Header().Set("Content-Type", "application/json")
+	_, _ = writer.Write(jsonResponse)
+}
+
+// sendRes отправляет результат запроса
+func sendRes(writer http.ResponseWriter, resStr string, events []Event, status int) {
+	response := struct {
+		Result string  `json:"result"`
+		Events []Event `json:"events"`
+	}{resStr, events}
+	responseJson, err := json.Marshal(response)
+	if err != nil {
+		sendErr(writer, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	writer.WriteHeader(status)
+	writer.Header().Set("Content-Type", "application/json")
+	_, _ = writer.Write(responseJson)
+}
+
+// checkEvent проверяет содержится ли событие в хранилище
+func (scope *Scope) checkEvent(e Event) bool {
+	for _, v := range scope.EventRepository.m[e.UserID] {
+		if v.ID == e.ID {
+			return true
+		}
+	}
+	return false
+}
+
+func ValidEvents(event Event) bool {
+	if event.ID <= 0 || event.UserID <= 0 || event.Title == "" || event.Description == "" {
+		return false
+	}
+	return true
+}
+
+func parseJSON(r *http.Request) (Event, error) {
+	var event Event
+	err := json.NewDecoder(r.Body).Decode(&event)
+	if err != nil {
+		return event, errors.New("cannot decode json")
+	}
+	return event, nil
+}
+
+              
+func (d *Date) UnmarshalJSON(input []byte) error {
+	var err error
+	d.date, err = time.Parse(`"2006-01-02"`, string(input))
+	return err
+}
+
+// String для типа Date                       
+func (d Date) String() string {
+	return d.date.String()
+}
+
+// MarshalJSON для типа Date                       
+func (d *Date) MarshalJSON() ([]byte, error) {
+	dateStr := d.date.Format("2006-01-02")
+	return json.Marshal(dateStr)
+}
+
+func (scope *Scope) DayEventsFunc(userID int, date time.Time) ([]Event, error) {
+	scope.EventRepository.RWMutex.RLock()
+	scope.EventRepository.RWMutex.RUnlock()
 
 	var result []Event
 
 	var allUserEvents []Event
-	allUserEvents = s.eventStore.m[userID]
+	allUserEvents = scope.EventRepository.m[userID]
 	if allUserEvents == nil {
 		return nil, errors.New("unknown user_id")
 	}
@@ -286,37 +309,35 @@ func (s *Scope) EventsForDayFunc(userID int, date time.Time) ([]Event, error) {
 	return result, nil
 }
 
-// Обработчик для /events_for_day
-func (s *Scope) EventsForDay(w http.ResponseWriter, r *http.Request) {
-	s.logger.Println(r.URL)
+func (scope *Scope) DayEvents(w http.ResponseWriter, r *http.Request) {
+	scope.logger.Println(r.URL)
 	if r.Method != http.MethodGet {
-		sendError(w, "Not correct method", http.StatusBadRequest)
+		sendErr(w, "Not correct method", http.StatusBadRequest)
 		return
 	}
 
 	userID, err := strconv.Atoi(r.URL.Query().Get("user_id"))
 	date, err := time.Parse("2006-01-02", r.URL.Query().Get("date"))
 	if err != nil {
-		sendError(w, "Incorrect args", http.StatusBadRequest)
+		sendErr(w, "Incorrect args", http.StatusBadRequest)
 		return
 	}
-	events, err := s.EventsForDayFunc(userID, date)
+	events, err := scope.DayEventsFunc(userID, date)
 	if err != nil {
-		sendError(w, err.Error(), http.StatusInternalServerError)
+		sendErr(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	sendResult(w, "Success", events, http.StatusOK)
+	sendRes(w, "Success", events, http.StatusOK)
 }
 
-// Возвращает события с разницей не более чем в неделю от заданной даты
-func (s *Scope) EventsForWeekFunc(userID int, date time.Time) ([]Event, error) {
-	s.eventStore.RWMutex.RLock()
-	s.eventStore.RWMutex.RUnlock()
+func (scope *Scope) WeekEventsFunc(userID int, date time.Time) ([]Event, error) {
+	scope.EventRepository.RWMutex.RLock()
+	scope.EventRepository.RWMutex.RUnlock()
 
 	var result []Event
 
 	var allUserEvents []Event
-	allUserEvents = s.eventStore.m[userID]
+	allUserEvents = scope.EventRepository.m[userID]
 	if allUserEvents == nil {
 		return nil, errors.New("unknown user_id")
 	}
@@ -333,37 +354,35 @@ func (s *Scope) EventsForWeekFunc(userID int, date time.Time) ([]Event, error) {
 	return result, nil
 }
 
-// Обработчик для /events_for_week
-func (s *Scope) EventsForWeek(w http.ResponseWriter, r *http.Request) {
-	s.logger.Println(r.URL)
+func (scope *Scope) WeekEvents(w http.ResponseWriter, r *http.Request) {
+	scope.logger.Println(r.URL)
 	if r.Method != http.MethodGet {
-		sendError(w, "Not correct method", http.StatusBadRequest)
+		sendErr(w, "Not correct method", http.StatusBadRequest)
 		return
 	}
 
 	userID, err := strconv.Atoi(r.URL.Query().Get("user_id"))
 	date, err := time.Parse("2006-01-02", r.URL.Query().Get("date"))
 	if err != nil {
-		sendError(w, "Incorrect args", http.StatusBadRequest)
+		sendErr(w, "Incorrect args", http.StatusBadRequest)
 		return
 	}
-	events, err := s.EventsForWeekFunc(userID, date)
+	events, err := scope.WeekEventsFunc(userID, date)
 	if err != nil {
-		sendError(w, err.Error(), http.StatusInternalServerError)
+		sendErr(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	sendResult(w, "Success", events, http.StatusOK)
+	sendRes(w, "Success", events, http.StatusOK)
 }
 
-// Возвращает события в заданном месяце
-func (s *Scope) EventsForMonthFunc(userID int, date time.Time) ([]Event, error) {
-	s.eventStore.RWMutex.RLock()
-	s.eventStore.RWMutex.RUnlock()
+func (scope *Scope) MonthEventsFunc(userID int, date time.Time) ([]Event, error) {
+	scope.EventRepository.RWMutex.RLock()
+	scope.EventRepository.RWMutex.RUnlock()
 
 	var result []Event
 
 	var allUserEvents []Event
-	allUserEvents = s.eventStore.m[userID]
+	allUserEvents = scope.EventRepository.m[userID]
 	if allUserEvents == nil {
 		return nil, errors.New("unknown user_id")
 	}
@@ -376,65 +395,28 @@ func (s *Scope) EventsForMonthFunc(userID int, date time.Time) ([]Event, error) 
 	return result, nil
 }
 
-// Обработчик для /events_for_month
-func (s *Scope) EventsForMonth(w http.ResponseWriter, r *http.Request) {
-	s.logger.Println(r.URL)
+func (scope *Scope) MonthEvents(w http.ResponseWriter, r *http.Request) {
+	scope.logger.Println(r.URL)
 	if r.Method != http.MethodGet {
-		sendError(w, "Not correct method", http.StatusBadRequest)
+		sendErr(w, "Not correct method", http.StatusBadRequest)
 		return
 	}
 
 	userID, err := strconv.Atoi(r.URL.Query().Get("user_id"))
 	date, err := time.Parse("2006-01-02", r.URL.Query().Get("date"))
 	if err != nil {
-		sendError(w, "Incorrect args", http.StatusBadRequest)
+		sendErr(w, "Incorrect args", http.StatusBadRequest)
 		return
 	}
-	events, err := s.EventsForMonthFunc(userID, date)
+	events, err := scope.MonthEventsFunc(userID, date)
 	if err != nil {
-		sendError(w, err.Error(), http.StatusInternalServerError)
+		sendErr(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	sendResult(w, "Success", events, http.StatusOK)
-}
-
-// Проверяет корректность заданного события
-func EventIsValid(event Event) bool {
-	if event.ID <= 0 || event.UserID <= 0 || event.Title == "" || event.Description == "" {
-		return false
-	}
-	return true
-}
-
-// Парсит событие из тела запроса
-func parseJson(r *http.Request) (Event, error) {
-	var event Event
-	err := json.NewDecoder(r.Body).Decode(&event)
-	if err != nil {
-		return event, errors.New("cannot decode json")
-	}
-	return event, nil
-}
-
-// Unmarshal() для типа MyDate
-func (d *MyDate) UnmarshalJSON(input []byte) error {
-	var err error
-	d.date, err = time.Parse(`"2006-01-02"`, string(input))
-	return err
-}
-
-// String() для типа MyDate
-func (d MyDate) String() string {
-	return d.date.String()
-}
-
-// Marhsal для типа MyDate
-func (d *MyDate) MarshalJSON() ([]byte, error) {
-	dateStr := d.date.Format("2006-01-02")
-	return json.Marshal(dateStr)
+	sendRes(w, "Success", events, http.StatusOK)
 }
 
 func main() {
-	scope := InitScope()
-	scope.startServer()
+	scope := CreateScope()
+	scope.startingServer()
 }
