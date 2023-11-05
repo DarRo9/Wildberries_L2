dd5d3547ef9248e3f55bbefe6f34becf3a203689
DarRo9 <124833469+DarRo9@users.noreply.github.com>
2023-11-05 21:16:11 +0300

    Update task.go

--- develop/dev08/task.go
+++ develop/dev08/task.go
@@ -1,9 +1,7 @@
 /*
 Взаимодействие с ОС
 
-
 Необходимо реализовать свой собственный UNIX-шелл-утилиту с поддержкой ряда простейших команд:
-
 
 - cd <args> - смена директории (в качестве аргумента могут быть то-то и то)
 - pwd - показать путь до текущего каталога
@@ -11,14 +9,9 @@
 - kill <args> - "убить" процесс, переданный в качесте аргумента (пример: такой-то пример)
 - ps - выводит общую информацию по запущенным процессам в формате *такой-то формат*
 
-
-
-
 Так же требуется поддерживать функционал fork/exec-команд
 
-
 Дополнительно необходимо поддерживать конвейер на пайпах (linux pipes, пример cmd1 | cmd2 | .... | cmdN).
-
 
 *Шелл — это обычная консольная программа, которая будучи запущенной, в интерактивном сеансе выводит некое приглашение 
 в STDOUT и ожидает ввода пользователя через STDIN. Дождавшись ввода, обрабатывает команду согласно своей логике 
@@ -29,30 +22,16 @@ package main
 import (
 	"bufio"
 	"fmt"
-	gops "github.com/mitchellh/go-ps"
 	"log"
 	"os"
 	"os/exec"
 	"strconv"
 	"strings"
+
+	go_ps "github.com/mitchellh/go-ps"
 )
 
-// Выводит аргумент в shell
-func echo(arg string) {
-	fmt.Println(arg)
-}
-
-// Выводит рабочую директорию в shell
-func pwd() {
-	path, err := os.Getwd()
-	if err != nil {
-		log.Println(err)
-	} else {
-		fmt.Println(path)
-	}
-}
-
-// Меняет рабочую директорию
+// cd меняет рабочую директорию
 func cd(dir string) {
 	err := os.Chdir(dir)
 	if err != nil {
@@ -60,8 +39,8 @@ func cd(dir string) {
 	}
 }
 
-// Запускает новый процесс, с соответствующими аргументами
-func forkExec(arg string) {
+// new_process запускает новый процесс, с соответствующими аргументами
+func new_process(arg string) {
 	args := strings.Split(arg, " ")
 
 	if len(args) < 1 {
@@ -74,9 +53,9 @@ func forkExec(arg string) {
 	}
 }
 
-// Выводит список процессов в shell
+// ps выводит список процессов в shell
 func ps() {
-	pcs, err := gops.Processes()
+	pcs, err := go_ps.Processes()
 	if err != nil {
 		fmt.Println("Error: ", err)
 	}
@@ -86,7 +65,35 @@ func ps() {
 	}
 }
 
-// Убивает процесс по PID
+// echo выводит аргумент в shell
+func echo(arg string) {
+	fmt.Println(arg)
+}
+
+// pwd выводит рабочую директорию в shell
+func pwd() {
+	path, err := os.Getwd()
+	if err != nil {
+		log.Println(err)
+	} else {
+		fmt.Println(path)
+	}
+}
+
+// shell запускает бесконечный цикл обработки
+func shell() {
+	sc := bufio.NewScanner(os.Stdin)
+	for fmt.Print(">"); sc.Scan(); fmt.Print(">") {
+		cmd := sc.Text()
+		if cmd == "quit" {
+			break
+		} else {
+			сommand_processing(cmd)
+		}
+	}
+}
+
+// kill завершает процесс по PID
 func kill(pidStr string) {
 	pid, err := strconv.Atoi(pidStr)
 	if err != nil {
@@ -105,8 +112,8 @@ func kill(pidStr string) {
 	}
 }
 
-// Обрабатывает команду, введенную в shell
-func execCmd(cmd string) {
+// сommand_processing обрабатывает команду, введенную в shell
+func сommand_processing(cmd string) {
 	switch strings.Split(cmd, " ")[0] {
 	case "echo":
 		echo(strings.Replace(cmd, "echo ", "", 1))
@@ -115,26 +122,13 @@ func execCmd(cmd string) {
 	case "cd":
 		cd(strings.Replace(cmd, "cd ", "", 1))
 	case "fork-exec":
-		forkExec(strings.Replace(cmd, "fork-exec ", "", 1))
+		new_process(strings.Replace(cmd, "fork-exec ", "", 1))
 	case "ps":
 		ps()
 	case "kill":
 		kill(strings.Replace(cmd, "kill ", "", 1))
 	default:
 		fmt.Println("Error: Unknown command")
-	}
-}
-
-// Бесконечный цикл обработки shell
-func shell() {
-	sc := bufio.NewScanner(os.Stdin)
-	for fmt.Print(">"); sc.Scan(); fmt.Print(">") {
-		cmd := sc.Text()
-		if cmd == "quit" {
-			break
-		} else {
-			execCmd(cmd)
-		}
 	}
 }
 
