96c6d61e07b43f81f08a06a31294435923efba11
DarRo9 <124833469+DarRo9@users.noreply.github.com>
2023-11-05 22:10:38 +0300

    Update task.go

--- develop/dev09/task.go
+++ develop/dev09/task.go
@@ -3,6 +3,7 @@
 
 Реализовать утилиту wget с возможностью скачивать сайты целиком.
 */
+
 package main
 
 import (
@@ -13,35 +14,52 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
-	url2 "net/url"
 	"os"
 	"strconv"
 	"strings"
 	"time"
+
+	url "net/url"
 )
 
-/*
-Реализовать утилиту wget с возможностью скачивать сайты целиком.
-*/
+// launch_wget создает рабочую директорию и выполняет wget
+func launch_wget(url string, outPath string, depth int, timeout time.Duration) {
+	if depth < 1 {
+		log.Fatal("Некорректное значение")
+	}
 
-// Функция скачивает страницу по url
-func downloadPage(url string) []byte {
-	resp, err := http.Get(url)
+	wd, _ := os.Getwd()
+	err := os.Mkdir(wd+`/`+outPath, os.ModeDir)
 	if err != nil {
-		log.Println("Cannot download page with url = ", url)
+		if os.IsNotExist(err) {
+			log.Fatal(err)
+		}
+	}
+	err = os.Chdir(outPath)
+	if err != nil {
+		log.Fatal("Невозможно изменить рабочий каталог")
+	}
+	make_recursion(url, url, depth, 0, 1, timeout)
+}
+
+// loadPage загружает и сохраняет страницу по url
+func loadPage(address string) []byte {
+	resp, err := http.Get(address)
+	if err != nil {
+		log.Println("Невозможно загрузить страницу по URL", address)
 		return nil
 	}
 	data, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		log.Println("Cannot read page data with url = ", url)
+		log.Println("Невозможно прочитать данные страницы с URL-адресом", address)
 	}
 
 	return data
 }
 
-// Функция записывает данные в файл
-func writeToFile(data []byte, filename string) error {
-	f, err := os.Create(filename)
+// writeInformation записывает информацию в файл
+func writeInformation(data []byte, file string) error {
+	f, err := os.Create(file)
 	if err != nil {
 		return err
 	}
@@ -53,40 +71,20 @@ func writeToFile(data []byte, filename string) error {
 	return nil
 }
 
-// Функция создает рабочую директорию и выполняет wget
-func wget(url string, outPath string, depth int, timeout time.Duration) {
-	if depth < 1 {
-		log.Fatal("Incorrect depth")
-	}
-
-	wd, _ := os.Getwd()
-	err := os.Mkdir(wd+`\`+outPath, os.ModeDir)
-	if err != nil {
-		if os.IsNotExist(err) {
-			log.Fatal("Cannot create directory")
-		}
-	}
-	err = os.Chdir(outPath)
-	if err != nil {
-		log.Fatal("Cannot change work dir")
-	}
-	wgetRec(url, url, depth, 0, 1, timeout)
-}
-
-// Реализация wget (рекурсивная)
-func wgetRec(rootUrl string, curUrl string, depth int, curDepth int, pageInd int, timeout time.Duration) {
+// make_recursion реализет wget
+func make_recursion(rootUrl string, curUrl string, depth int, curDepth int, pageInd int, timeout time.Duration) {
 	if curDepth == depth {
 		return
 	}
 
-	data := downloadPage(curUrl)
+	data := loadPage(curUrl)
 
 	if data != nil {
-		links := getLinks(data)
+		links := sliceOfLinks(data)
 
 		linksNorm := make([]string, len(links))
 		copy(linksNorm, links)
-		normalizeLinks(linksNorm, rootUrl, curUrl)
+		formatLinks(linksNorm, rootUrl, curUrl)
 
 		for i := 0; i < len(links); i++ {
 			oldLink := []byte(links[i])
@@ -94,20 +92,20 @@ func wgetRec(rootUrl string, curUrl string, depth int, curDepth int, pageInd int
 			copy(data, bytes.ReplaceAll(data, oldLink, newLink))
 		}
 
-		err := writeToFile(data, strconv.Itoa(curDepth)+"_"+strconv.Itoa(pageInd)+".html")
+		err := writeInformation(data, strconv.Itoa(curDepth)+"_"+strconv.Itoa(pageInd)+".html")
 		if err != nil {
-			log.Println("Cannot write file: ", err)
+			log.Println("Невозможно записать файл: ", err)
 		}
 
 		for i := 0; i < len(linksNorm); i++ {
 			time.Sleep(timeout)
-			wgetRec(rootUrl, linksNorm[i], depth, curDepth+1, i, timeout)
+			make_recursion(rootUrl, linksNorm[i], depth, curDepth+1, i, timeout)
 		}
 	}
 }
 
-// Функция возвращает слайс ссылок из тела заданной страницы
-func getLinks(body []byte) []string {
+// sliceOfLinks возвращает слайс ссылок из тела заданной страницы
+func sliceOfLinks(body []byte) []string {
 	var links []string
 	bodyReader := bytes.NewReader(body)
 	z := html.NewTokenizer(bodyReader)
@@ -132,25 +130,8 @@ func getLinks(body []byte) []string {
 	}
 }
 
-// Функция приводит ссылки к полному виду (с протоколом и доменом)
-func normalizeLinks(links []string, rootUrl string, parentUrl string) {
-	for i, link := range links {
-		if !strings.HasPrefix(link, "http://") && !strings.HasPrefix(link, "https://") {
-			if strings.HasPrefix(link, "/") {
-				url, err := url2.Parse(rootUrl)
-				if err != nil {
-					log.Println("Cannot parse url: ", url)
-				}
-				links[i] = url.Scheme + "://" + urlAddSuffixIfNeeded(url.Host) + link[1:]
-			} else {
-				links[i] = urlAddSuffixIfNeeded(parentUrl) + link
-			}
-		}
-	}
-}
-
-// Функция добавляет slash в конец ссылки
-func urlAddSuffixIfNeeded(url string) string {
+// AddSuffix добавляет / в конец ссылки
+func AddSuffix(url string) string {
 	if !strings.HasSuffix(url, "/") {
 		return url + "/"
 	} else {
@@ -158,18 +139,35 @@ func urlAddSuffixIfNeeded(url string) string {
 	}
 }
 
+// formatLinks приводит ссылки к полному виду (с протоколом и доменом)
+func formatLinks(links []string, rootUrl string, parentUrl string) {
+	for i, link := range links {
+		if !strings.HasPrefix(link, "http://") && !strings.HasPrefix(link, "https://") {
+			if strings.HasPrefix(link, "/") {
+				url, err := url.Parse(rootUrl)
+				if err != nil {
+					log.Println("Невозможно разобрать URL: ", url)
+				}
+				links[i] = url.Scheme + "://" + AddSuffix(url.Host) + link[1:]
+			} else {
+				links[i] = AddSuffix(parentUrl) + link
+			}
+		}
+	}
+}
+
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
+	scan := bufio.NewScanner(os.Stdin)
 
-	fmt.Println("Enter full url, example: https://example.com/")
-	scanner.Scan()
-	url := scanner.Text()
+	fmt.Println("Введите полный URL")
+	scan.Scan()
+	url := scan.Text()
 
-	fmt.Println("Enter full directory path to store html files")
-	scanner.Scan()
-	path := scanner.Text()
+	fmt.Println("Введите полный путь к каталогу для хранения html-файлов")
+	scan.Scan()
+	path := scan.Text()
 
-	wget(url, path, 2, 3)
+	launch_wget(url, path, 2, 3)
 
-	fmt.Println("Download completed")
+	fmt.Println("Загрузка завершена")
 }
